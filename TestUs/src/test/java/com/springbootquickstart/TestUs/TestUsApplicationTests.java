a0bb5a25942f49160a17fc36641934aaccf13b98
daniel kovbichenko <dandaniel149@gmail.com>
2024-02-06 11:10:15 +0200

    add simple start project

---
+++ TestUs/src/test/java/com/springbootquickstart/TestUs/TestUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootquickstart.TestUs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
