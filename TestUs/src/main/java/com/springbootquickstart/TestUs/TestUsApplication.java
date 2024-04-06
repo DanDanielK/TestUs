a36e70343df5b989f4a3ded9d4af9355663add04
Dor Kelner <frba100bw@gmail.com>
2024-04-06 14:52:25 +0300

    basic test creation interface (only title)

--- TestUs/src/main/java/com/springbootquickstart/TestUs/TestUsApplication.java
+++ TestUs/src/main/java/com/springbootquickstart/TestUs/TestUsApplication.java
@@ -4,14 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-
 @SpringBootApplication
 public class TestUsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestUsApplication.class, args);
-		
-	
+
 	}
 
 }
